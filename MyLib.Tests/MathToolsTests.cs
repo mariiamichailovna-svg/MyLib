e1b032b85d63fd79b2f3d47f75b6c7f1644f6158
Mariia <mariiamichailovna@gmail.com>
2026-01-15 17:18:45 +0300

    Branch A: change header

---
+++ MyLib.Tests/MathToolsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MyLib;
+
+namespace MyLib.Tests;
+
+public class MathToolsTests
+{
+    [Fact]
+    public void Add_Works()
+    {
+        Assert.Equal(7, MathTools.Add(3, 4));
+    }
+
+    [Fact]
+    public void Sub_Works()
+    {
+        Assert.Equal(-1, MathTools.Sub(3, 4));
+    }
+
+    [Fact]
+    public void Mul_Works()
+    {
+        Assert.Equal(12, MathTools.Mul(3, 4));
+    }
+}
---
+++ MyLib.Tests/StringToolsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MyLib;
+
+namespace MyLib.Tests;
+
+public class StringToolsTests
+{
+    [Fact]
+    public void Reverse_Works()
+    {
+        Assert.Equal("cba", StringTools.Reverse("abc"));
+    }
+
+    [Fact]
+    public void Reverse_Null_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => StringTools.Reverse(null!));
+    }
+
+    [Fact]
+    public void WordCount_Works()
+    {
+        Assert.Equal(3, StringTools.WordCount("one  two   three"));
+    }
+}
+
