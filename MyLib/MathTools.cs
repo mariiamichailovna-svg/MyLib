2b11c01688794f6766422e194a8f2d364a7ad565
mreem <mreem@MARIA>
2026-01-15 16:58:29 +0300

    Добавьте файлы проекта.

---
+++ MyLib/MathTools.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLib;
+
+/// <summary>
+/// Набор простых математических функций.
+/// </summary>
+public static class MathTools
+{
+    /// <summary>Складывает два целых числа.</summary>
+    public static int Add(int a, int b) => a + b;
+
+    /// <summary>Вычитает b из a.</summary>
+    public static int Sub(int a, int b) => a - b;
+
+    /// <summary>Умножает два целых числа.</summary>
+    public static int Mul(int a, int b) => a * b;
+}
---
+++ MyLib/StringTools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib;
+
+/// <summary>
+/// Утилиты для работы со строками.
+/// </summary>
+public static class StringTools
+{
+    /// <summary>Разворачивает строку задом наперёд.</summary>
+    /// <exception cref="ArgumentNullException">Если строка равна null.</exception>
+    public static string Reverse(string s)
+    {
+        if (s is null) throw new ArgumentNullException(nameof(s));
+        return new string(s.Reverse().ToArray());
+    }
+
+    /// <summary>Считает количество слов (разделители: пробелы/таб/переносы).</summary>
+    /// <exception cref="ArgumentNullException">Если строка равна null.</exception>
+    public static int WordCount(string s)
+    {
+        if (s is null) throw new ArgumentNullException(nameof(s));
+        return s.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
+
